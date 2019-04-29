1d5a8221ebe60813531c36429ff1bfb363734aa7
Oliver Ellmers <oliver@random42.com>
2019-04-29 16:03:43 +0100

    Game Manager Setup

---
+++ Assets/Scripts/AppSetup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppSetup : MonoBehaviour
+{
+    public AppData allAppData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(allAppData.StartScene);
+        
+    }
+
+
+    public AppData GetAppData()
+    {
+        return allAppData;
+    }
+
+    private void SetupUI() {
+        foreach (CanvasGroup cg in allAppData.UILayers) {
+            if (allAppData.StartScene != cg.gameObject.name)
+            {
+                cg.interactable = false;
+                cg.blocksRaycasts = false;
+                cg.alpha = 0;
+            }
+            else {
+                cg.interactable = true;
+                cg.blocksRaycasts = true;
+                cg.alpha = 1;
+            }
+            
+        }
+        
+    }
+}
